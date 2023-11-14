533f2c57c2680554ec7ee2f0f87d62d952460ab1
impath96 <impath96@gmail.com>
2023-11-14 15:05:12 +0900

    #1 - spring boot로 전환
    1. pom.xml 파일 수정
    잘못된 자바 설정 수정(java-version -> java.version)
    spring 4.3.8 버전을 4.3.9버전으로 upgrade 하기 위해
    spring boot 버전을 1.4.6에서 1.4.7로 변경
    - Last modified check of Resource created from Tomcat war:file: URL fails with FileNotFoundException
    
    2. web.xml 삭제
    - CharacterEncodingFilter의 경우 spring boot에서 자동으로 등록
    
    3. root-context.xml 에서 Mybatis, DataSource 빈 설정 삭제
    Mybatis의 Type Alias 설정이 동작하지 않아서 DataSource, Mybatis와 관련된 빈 설정을 root-context.xml에서 제거하고
    application.properties로 설정하도록 변경
    
    4. 일단은 기존 xml 방식 빈 설정을 유지할 거기 때문에 @ImportResource를 통해 root-context.xml, servlet-context.xml
    을 그대로 import
    
    5. properties 파일 내에 ${} 형식에서 값을 읽어오지 못하는 문제가 계속 발생해서 @PropertySource를 활용하기로 결정

---
+++ src/main/java/com/joinus/JoinUsApplication.java
@@ -0,0 +1,25 @@
+package com.joinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@ImportResource(value = {
+    "classpath:/WEB-INF/spring/root-context.xml",
+    "classpath:/WEB-INF/spring/appServlet/servlet-context.xml",
+})
+@PropertySource(value = {
+    "classpath:google.properties",
+    "classpath:dbconnect.properties",
+    "classpath:kakao.properties",
+    "classpath:mail.properties",
+})
+public class JoinUsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoinUsApplication.class, args);
+    }
+
+}
